5887db8168b6c959ac7e11e354145d1d960f7a5a
raul <product.raul@gmail.com>
2024-05-12 23:12:24 +0900

    chore: build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,20 @@
+import com.google.protobuf.gradle.id
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
+    id("com.google.protobuf") version "0.9.4"
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.spring") version "1.9.23"
 }
 
 group = "kr.argonaut"
 version = "0.0.1-SNAPSHOT"
+
+val protobufVersion = "3.24.0"
+val grpcKotlinVersion = "1.4.0"
+val grpcVersion = "1.60.1"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
@@ -23,18 +29,71 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("aws.sdk.kotlin:s3:1.0.0")
+    implementation("aws.sdk.kotlin:s3:1.0.0") {
+        exclude("com.squareup.okhttp3:okhttp")
+    }
+    implementation("org.kohsuke:github-api:1.321")
+
+    implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
+    implementation("io.grpc:grpc-protobuf:$grpcVersion")
+    implementation("com.google.protobuf:protobuf-kotlin:$protobufVersion")
+    runtimeOnly("io.grpc:grpc-netty-shaded:$grpcVersion")
+
+    implementation("aws.smithy.kotlin:http-client-engine-crt:1.2.2")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
-        jvmTarget = "17"
+tasks {
+    withType<Test> {
+        useJUnitPlatform()
     }
-}
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+    withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+            jvmTarget = "17"
+        }
+    }
+
+    sourceSets{
+        main {
+            proto {
+                srcDir("src/../aethalides/proto")
+            }
+        }
+        getByName("main"){
+            java {
+                srcDirs(
+                    "build/generated/source/proto/main/java",
+                    "build/generated/source/proto/main/kotlin"
+                )
+            }
+        }
+    }
+
+    protobuf {
+        protoc {
+            artifact = "com.google.protobuf:protoc:$protobufVersion"
+        }
+        plugins {
+            id("grpc") {
+                artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+            }
+            id("grpckt") {
+                artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk8@jar"
+            }
+        }
+        generateProtoTasks {
+            all().forEach {
+                it.plugins {
+                    id("grpc")
+                    id("grpckt")
+                }
+                it.builtins {
+                    id("kotlin")
+                }
+            }
+        }
+    }
 }
