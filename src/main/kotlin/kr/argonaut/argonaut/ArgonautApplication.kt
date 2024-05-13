b16cabc5bacaf2b6afa1b64b0acd2c4b21301a9c
지인호 [Raul] <product.raul@gmail.com>
2024-05-14 06:44:46 +0900

    feat: environment (#16)
    * chore: fix application.yml
    
    * feat: environment bean configuration

--- src/main/kotlin/kr/argonaut/argonaut/ArgonautApplication.kt
+++ src/main/kotlin/kr/argonaut/argonaut/ArgonautApplication.kt
@@ -1,10 +1,21 @@
 package kr.argonaut.argonaut
 
+import kr.argonaut.argonaut.domain.manifest.Environment
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
+@Configuration
 @SpringBootApplication
-class ArgonautApplication
+@EnableConfigurationProperties
+class ArgonautApplication {
+    @Bean
+    fun spigotEnvironment(@Value("\${spring.profiles.active}") profile: String): Environment =
+        Environment.fromString(profile)
+}
 
 fun main(args: Array<String>) {
     runApplication<ArgonautApplication>(*args)
